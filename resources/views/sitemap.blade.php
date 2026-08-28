b932ff4787c6e5c2b0fb19930d1820dd907feeeb
Arjun <jaluarjun442@gmail.com>
2026-08-28 17:32:47 +0530

    sitemap blaed changes

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -2,7 +2,6 @@
     $tools = config('tools');
 @endphp
 
-<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
     <url>
@@ -10,6 +9,14 @@
         <changefreq>weekly</changefreq>
         <priority>1.0</priority>
     </url>
+
+    @foreach($tools as $tool)
+        <url>
+            <loc>{{ url('/' . $tool['slug']) }}</loc>
+            <changefreq>monthly</changefreq>
+            <priority>0.8</priority>
+        </url>
+    @endforeach
 
     <url>
         <loc>{{ url('/about-us') }}</loc>
@@ -41,13 +48,4 @@
         <priority>0.5</priority>
     </url>
 
-    @foreach($tools as $tool)
-
-        <url>
-            <loc>{{ url('/' . $tool['slug']) }}</loc>
-            <changefreq>monthly</changefreq>
-            <priority>0.8</priority>
-        </url>
-
-    @endforeach
 </urlset>
