03475bfc7118931f700c5cf907f166fcd405ca08
Arjun <jaluarjun442@gmail.com>
2026-08-28 17:38:49 +0530

    sitemap change

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -4,7 +4,7 @@
     echo '<?xml version="1.0" encoding="UTF-8"?>';
 @endphp
 
-<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 
     <url>
         <loc>{{ url('/') }}</loc>
