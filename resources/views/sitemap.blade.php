66b9db5704f335333ad581912c0627121b459848
Arjun <jaluarjun442@gmail.com>
2026-08-28 17:36:57 +0530

    sitemap tag changes

--- resources/views/sitemap.blade.php
+++ resources/views/sitemap.blade.php
@@ -1,9 +1,10 @@
 @php
     $tools = config('tools');
+
+    echo '<?xml version="1.0" encoding="UTF-8"?>';
 @endphp
 
-{{ '<?xml version="1.0" encoding="UTF-8"?>' }}
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
 
     <url>
         <loc>{{ url('/') }}</loc>
